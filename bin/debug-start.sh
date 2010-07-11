2ddc53c4a52a94670efab598b3d54d24a21635a9
Jennifer Morgan <jennifermorgan2001@gmail.com>
2010-07-11 23:11:39 +0000

    got passing json to work just have to work on it some more to pass back json

---
+++ bin/debug-start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#REM Excerpted from "Programming Clojure",
+#REM published by The Pragmatic Bookshelf.
+#REM Copyrights apply to this code. It may not be used to create training material, 
+#REM courses, books, articles, and the like. Contact us if you are in doubt.
+#REM We make no guarantees that this code is fit for any purpose. 
+#REM Visit http://www.pragmaticprogrammer.com/titles/shcloj for more book information.
+#rem 
+#For Dev
+#../jdk1.6.0_20/bin/java -cp .:lib/commons-io-1.4.jar:lib/commons-fileupload-1.2.1.jar:lib/commons-codec-1.3.jar:lib/jline-0.9.94.jar:lib/clojure.jar:lib/clojure-contrib.jar:lib/ant.jar:lib/ant-launcher.jar:lib/compojure.jar:lib/hsqldb.jar:lib/jetty-6.1.14.jar:lib/jetty-util-6.1.14.jar:lib/servlet-api-2.5-6.1.14.jar:src jline.ConsoleRunner clojure.lang.Repl src/com/web/html/start.clj
+
+# nohup ./bin/start.sh &
+../jdk1.6.0_20/bin/java -cp .:lib/commons-io-1.4.jar:lib/commons-fileupload-1.2.1.jar:lib/commons-codec-1.3.jar:lib/jline-0.9.94.jar:lib/clojure.jar:lib/clojure-contrib.jar:lib/ant.jar:lib/ant-launcher.jar:lib/compojure.jar:lib/hsqldb.jar:lib/jetty-6.1.14.jar:lib/jetty-util-6.1.14.jar:lib/servlet-api-2.5-6.1.14.jar:src  clojure.main.repl 
+#src/com/web/html/start.clj
+
+
